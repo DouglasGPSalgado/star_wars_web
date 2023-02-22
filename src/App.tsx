2d9db761097c80a16e5f0222e2e0a225c7842125
DouglasGPSalgado <douglasgsk8@gmail.com>
2023-02-21 23:46:13 -0300

    feat: Adicionado tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router";
+import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
+    <ThemeProvider theme={LightTheme}>
     <BrowserRouter>
       <AppRoutes/>
     </BrowserRouter>
+    </ThemeProvider>
   );
 };
